16bdadc9a5a8f403d93bd2cfa35e11267790e027
Joanna Konder <149900886+nonakonder@users.noreply.github.com>
2024-06-05 22:57:17 -0300

    Scanner class study

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -1,3 +1,6 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) {
        
@@ -7,13 +10,30 @@ public class ContaTerminal {
         //string agencia = numeroAgencia + "-" + digitoVerificador; 
         //string nomeCliente = "MARIO ANDRADE";
         //Double SaldoBancario = 234.8; 
+//--------------------------------------------------
+        //int numeroConta = Integer.valueOf(args[0]);
+        //String numeroAgencia = args[1];
+        //char digitoVerificador = args[2].charAt(0);
+        //String agencia = numeroAgencia + "-" + digitoVerificador;
+       //String nomeCliente = args[3];
+       // double saldoBancario = Double.valueOf(args[4]);
+//-----------------------------------------------------------
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
 
-        int numeroConta = Integer.valueOf(args[0]);
-        String numeroAgencia = args[1];
-        char digitoVerificador = args[2].charAt(0);
-        String agencia = numeroAgencia + "-" + digitoVerificador;
-        String nomeCliente = args[3];
-        double saldoBancario = Double.valueOf(args[4]);
+        System.out.println("Digite o número da sua Conta:");
+        int numeroConta = scanner.nextInt();
+
+        System.out.println("Digite o número da sua agência:");
+        String agencia = scanner.next();
+        String numeroAgencia= agencia.substring(0, agencia.length() - 1);
+        char digitoVerificador = agencia.charAt(agencia.length() - 1);
+
+        System.out.println("Digite sem nome e sobrenome:");
+        String nomeCliente = scanner.next();
+            
+        double saldoBancario = 58.87;
+
+        scanner.close();
 
         System.out.println("Olá "+ nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + numeroConta + " e seu saldo de " + saldoBancario + " já está disponível para saque");
 
