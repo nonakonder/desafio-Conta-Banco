2c62d972eab21c218c695bc0dae04b5b87226c54
Joanna Konder <149900886+nonakonder@users.noreply.github.com>
2024-06-05 22:17:21 -0300

    burra demais

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -9,10 +9,10 @@ public class ContaTerminal {
         //Double SaldoBancario = 234.8; 
 
         int numeroConta = Integer.valueOf(args[0]);
-        string numeroAgencia = args[1];
-        string digitoVerificador = args[2];
-        string agencia = numeroAgencia + "-" + digitoVerificador;
-        string nomeCliente = args[3];
+        String numeroAgencia = args[1];
+        char digitoVerificador = args[2].charAt(0);
+        String agencia = numeroAgencia + "-" + digitoVerificador;
+        String nomeCliente = args[3];
         double saldoBancario = Double.valueOf(args[4]);
 
         System.out.println("Olá "+ nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + numeroConta + " e seu saldo de " + saldoBancario + " já está disponível para saque");
