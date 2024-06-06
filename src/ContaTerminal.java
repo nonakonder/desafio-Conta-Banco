b3e5e4f6daee96f42a046f0242d9ef719e03c4eb
Joanna Konder <149900886+nonakonder@users.noreply.github.com>
2024-06-05 22:06:57 -0300

    primeiro commit

---
+++ src/ContaTerminal.java
@@ -0,0 +1,21 @@
+public class ContaTerminal {
+    public static void main(String[] args) {
+       
+        //int numero = 1021;
+        //string numeroAgencia = "067";
+        //char digitoVerificador = '8';
+        //string agencia = numeroAgencia + "-" + digitoVerificador; 
+        //string nomeCliente = "MARIO ANDRADE";
+        //Double SaldoBancario = 234.8; 
+
+        int numeroConta = Integer.valueOf(args[0]);
+        string numeroAgencia = args[1];
+        char digitoVerificador = args[2].charAt(0);
+        string agencia = numeroAgencia + "-" + digitoVerificador;
+        string nomeCliente = args[3];
+        double saldoBancario = Double.valueOf(args[4]);
+
+        System.out.println("Olá "+ nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + numeroConta + " e seu saldo de " + saldoBancario + " já está disponível para saque");
+
+    }
+}
